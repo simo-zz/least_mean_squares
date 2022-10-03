27511ec73da14b3406669a3b2a2f680ceeb5ce03
simon <sazzalin@dares.tech>
2022-10-03 17:41:19 +0200

    Added space after commas into .m files

--- matlab_octave/float/lms_example.m
+++ matlab_octave/float/lms_example.m
@@ -8,24 +8,24 @@ N = 1000;
 % samples index vector
 n = 1:N;
 
-% v = input, noise 
+% v = input,  noise 
 vVar = 0.5; 
-v = vVar.*randn(N, 1); 
+v = vVar.*randn(N,  1); 
 
 % H filter 
 bH = [1]; 
 aH = [1 -0.8 0.5];
 
 % x = H filter output 
-x = filter(bH, aH, v); 
+x = filter(bH,  aH,  v); 
 
 % y = W adaptive filter output
 % since the filtering task is iterative 
-% because the coefficients will be updated each time,
+% because the coefficients will be updated each time, 
 % declare it as a vector of N elements. 
 % updated output values will be inserted in it.
-y = zeros(N, 1); 
-err = zeros(N,1);
+y = zeros(N,  1); 
+err = zeros(N, 1);
 
 % Filtering related parameters
 % temporary buffer / samples size
@@ -33,25 +33,25 @@ M = 5;
 m = 1:M;
 
 % Unknown system impulse response
-s = randn(M, 1);
+s = randn(M,  1);
 
 % Reference signal
-d = filter(s, 1, x);
+d = filter(s,  1,  x);
 
 % x samples vector used for filtering
-xtmp = zeros(M,1);
+xtmp = zeros(M, 1);
 
 % W time domain vector
-wLMS = zeros(M,1);
+wLMS = zeros(M, 1);
 
 % LMS error and output vector.Use it to update y vector of size N
-eLMS = zeros(M,1);
-yLMS = zeros(M,1); 
+eLMS = zeros(M, 1);
+yLMS = zeros(M, 1); 
 uLMS = 0.5;
 
 fig = figure(); 
 C = jet(N);
-kc = linspace(0.25,1,N); 
+kc = linspace(0.25, 1, N); 
 
 for k = n
 
@@ -63,32 +63,32 @@ for k = n
     err(k) = eLMS;
     wLMS = wLMS + uLMS * (eLMS .* xtmp)./M;
     
-    plot(m,wLMS,'LineWidth',2,'Color',kc(k).*C(k,:));
+    plot(m, wLMS, 'LineWidth', 2, 'Color', kc(k).*C(k, :));
     hold on; grid on;
 
  end
  
-scatter(m,s,100,'MarkerFaceColor',[1 0.95 0.2],'MarkerEdgeColor', [1 0 0]);
+scatter(m, s, 100, 'MarkerFaceColor', [1 0.95 0.2], 'MarkerEdgeColor',  [1 0 0]);
 hold on;
 title('w evolution vs s');
-print('wLMS_vs_s.png','-dpng');
+print('wLMS_vs_s.png', '-dpng');
 waitfor(fig);
 
 fig=figure(); 
-subplot(5,1,1)
-plot(n,v,'LineWidth',2,'Color',[0 0 1]); grid on;
+subplot(5, 1, 1)
+plot(n, v, 'LineWidth', 2, 'Color', [0 0 1]); grid on;
 legend('v = Input');
-subplot(5,1,2)
-plot(n,x, 'LineWidth',2,'Color', [1 0.5412 0.2]); grid on;
+subplot(5, 1, 2)
+plot(n, x,  'LineWidth', 2, 'Color',  [1 0.5412 0.2]); grid on;
 legend('x = H filter output');
-subplot(5,1,3)
-plot(n,d,'LineWidth',2,'Color',[0 0 0]);grid on;
+subplot(5, 1, 3)
+plot(n, d, 'LineWidth', 2, 'Color', [0 0 0]);grid on;
 legend('d = System output referencesignal');
-subplot(5,1,4)
-plot(n,y,'LineWidth',2,'Color',[0 1 0]); grid on;
+subplot(5, 1, 4)
+plot(n, y, 'LineWidth', 2, 'Color', [0 1 0]); grid on;
 legend('y = Adaptive filter output');
-subplot(5,1,5)
-plot(n,err,'LineWidth',2,'Color',[1 0 0]); grid on;
+subplot(5, 1, 5)
+plot(n, err, 'LineWidth', 2, 'Color', [1 0 0]); grid on;
 legend('e = d - y = Error');
-print('data_plots.png','-dpng'); 
+print('data_plots.png', '-dpng'); 
 waitfor(fig);
--- matlab_octave/int/lms_example.m
+++ matlab_octave/int/lms_example.m
@@ -14,23 +14,23 @@ N = 500;
 % samples index vector
 n = 1:N;
 
-% v = input, noise 
-v = randi([MinVal, MaxVal], N, 1); 
+% v = input,  noise 
+v = randi([MinVal,  MaxVal],  N,  1); 
 
 % H filter 
 bH = [1]; 
 aH = [100 -80 50];
 
 % x = H filter output 
-x = filter(bH, aH, v); 
+x = filter(bH,  aH,  v); 
 
 % y = W adaptive filter output
 % since the filtering task is iterative 
-% because the coefficients will be updated each time,
+% because the coefficients will be updated each time, 
 % declare it as a vector of N elements. 
 % updated output values will be inserted in it.
-y = zeros(N, 1); 
-err = zeros(N,1);
+y = zeros(N,  1); 
+err = zeros(N, 1);
 
 % Filtering related parameters
 % temporary buffer / samples size
@@ -38,25 +38,25 @@ M = 5;
 m = 1:M;
 
 % Unknown system impulse response
-s = randi([MinVal, MaxVal], M, 1);
+s = randi([MinVal,  MaxVal],  M,  1);
 
 % Reference signal
-d = filter(s, 1, x);
+d = filter(s,  1,  x);
 
 % x samples vector used for filtering
-xtmp = zeros(M, 1);
+xtmp = zeros(M,  1);
 
 % W time domain vector
-wLMS = zeros(M, 1);
+wLMS = zeros(M,  1);
 
 % LMS error and output vector.Use it to update y vector of size N
-eLMS = zeros(M,1);
-yLMS = zeros(M,1); 
+eLMS = zeros(M, 1);
+yLMS = zeros(M, 1); 
 uLMS = 1./uLMSDen
 
 fig = figure(); 
 C = jet(N);
-kc = linspace(0.25,1,N); 
+kc = linspace(0.25, 1, N); 
 
 for k = n
 
@@ -74,37 +74,37 @@ for k = n
        break;
     end
     
-    plot(m,wLMS,'LineWidth',2,'Color',kc(k).*C(k,:)); grid on; hold on;
+    plot(m, wLMS, 'LineWidth', 2, 'Color', kc(k).*C(k, :)); grid on; hold on;
 
  end
 
-scatter(m,s,100,'MarkerFaceColor',[1 0.95 0.2],'MarkerEdgeColor', [1 0 0]);
+scatter(m, s, 100, 'MarkerFaceColor', [1 0.95 0.2], 'MarkerEdgeColor',  [1 0 0]);
 hold on;
 title('w evolution vs s');
-print('wLMS_vs_s.png','-dpng');
+print('wLMS_vs_s.png', '-dpng');
 waitfor(fig);
 
 fig=figure(); 
-subplot(5,1,1)
-plot(n,v,'LineWidth',2,'Color',[0 0 1]); grid on;
+subplot(5, 1, 1)
+plot(n, v, 'LineWidth', 2, 'Color', [0 0 1]); grid on;
 legend('v = Input');
-subplot(5,1,2)
-plot(n,x, 'LineWidth',2,'Color', [1 0.5412 0.2]); grid on;
+subplot(5, 1, 2)
+plot(n, x,  'LineWidth', 2, 'Color',  [1 0.5412 0.2]); grid on;
 legend('x = H filter output');
-subplot(5,1,3)
-plot(n,d,'LineWidth',2,'Color',[0 0 0]);grid on;
-legend('d = System output, reference signal');
-subplot(5,1,4)
-plot(n,y,'LineWidth',2,'Color',[0 1 0]); grid on;
+subplot(5, 1, 3)
+plot(n, d, 'LineWidth', 2, 'Color', [0 0 0]);grid on;
+legend('d = System output,  reference signal');
+subplot(5, 1, 4)
+plot(n, y, 'LineWidth', 2, 'Color', [0 1 0]); grid on;
 legend('y = Adaptive filter output');
-subplot(5,1,5)
-plot(n,err,'LineWidth',2,'Color',[1 0 0]); grid on;
+subplot(5, 1, 5)
+plot(n, err, 'LineWidth', 2, 'Color', [1 0 0]); grid on;
 legend('e = d - y = Error');
-print('data_plots.png','-dpng');
+print('data_plots.png', '-dpng');
 waitfor(fig);
 
 disp('s vs wLMS');
-disp([s, wLMS]);
+disp([s,  wLMS]);
 
 disp(['y bits = ' num2str(ceil(log2(max(abs(y)))))]);
 disp(['err bits = ' num2str(ceil(log2(max(abs(err)))))]);
